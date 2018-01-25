9bda1ed98f16d769ea3349acf340b02a0b7f62d6
shawn <axe2781@gmail.com>
2018-01-25 10:39:24 -0500

    ho

---
+++ Telgram/Telgram/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Telgram.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
